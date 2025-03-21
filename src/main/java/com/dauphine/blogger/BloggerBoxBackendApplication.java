fadd5a9d11af0484129dff390eb75a109f950c56
Simon Regenwetter <64084644+simrgt@users.noreply.github.com>
2025-03-21 14:06:39 +0100

    add documentation via Swagger

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -3,7 +3,19 @@ package com.dauphine.blogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Contact;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Blogger box backend",
+		description = "Blogger box endpoints and apis",
+		contact = @Contact(name = "Simon", email = "simon.regenwetter@dauphine.eu"),
+		version = "1.0.0"
+	)
+)
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dauphine/blogger/controller/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controller/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.dauphine.blogger.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 @RestController
+@Tag(
+    name = "Hello World API",
+    description = "Hello World endpoints"
+)
 public class HelloWorldController {
 
     @GetMapping("hello-world")
@@ -18,7 +26,14 @@ public class HelloWorldController {
     }
 
     @GetMapping("hello/{name}")
-    public String helloPath(@RequestParam String name) {
+    @Operation(
+        summary = "Hello by name endpoint",
+        description = "Returns 'Hello {name}' by path variable"
+    )
+    public String helloPath(
+        @Parameter(description = "Name to greet")
+        @PathVariable String name
+    ) {
         return "Hello " + name;
     }
 }
